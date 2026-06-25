eabf8b969ddc417f05688b8e9a302d17f5c4287b
Jaimeen <jaimeen@gmail.com>
2026-06-25 10:09:25 +0530

    adding digits of pi

---
+++ pi/script.js
@@ -0,0 +1,34 @@
+let arrData = "3.141592653589793238462643383279"
+let currInd = 0;
+
+function onBodyLoad()
+{
+    // alert("A");
+    addEventListener()
+}
+
+function addEventListener(){
+    document.querySelector(".mainCont").addEventListener("click", nextDigit);
+
+    document.querySelector(".resetBtnArea").addEventListener("click", resetAll);    
+}
+
+function resetAll(){
+    console.log("resetBtnArea")
+    let yesNoConf = confirm("Are you sure you want to reset?");
+    
+    if (yesNoConf){
+        currInd = -1;
+        nextDigit();
+    }
+}
+
+function nextDigit(){
+    currInd++;
+
+    document.querySelector(".resetBtnArea").innerText = String(currInd-1).trim();
+
+    document.querySelector(".mainContent").innerText = arrData.charAt(currInd);
+    // document.querySelector(".mainCont").style.fontSize = "21.7em";
+
+}
