b041733bf7bb4c141a665867d505a89d1d4f4fd4
Jaimeen <jaimeen@gmail.com>
2026-06-27 11:09:59 +0530

    updates to pi - ability to select digit to go back to

--- pi/script.js
+++ pi/script.js
@@ -5,12 +5,45 @@ function onBodyLoad()
 {
     // alert("A");
     addEventListener()
+    populateSelectOption();
+}
+
+function populateSelectOption(){
+    let selectElem = document.querySelector("#digitCountSelect");
+    let numDigits = arrData.length - 2; // Subtract 2 to exclude "3."
+    for (let i = 1; i <= numDigits; i++) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.textContent = i;
+        selectElem.appendChild(option);
+    }
+}
+
+function onDigitCountChange(){
+    document.querySelector("#selectCont").classList.add("hdn");
+    let nDigit = document.querySelector("#digitCountSelect").value;
+    document.querySelector(".digitCountCont").innerText = String(nDigit).trim();
+    currInd = parseInt(nDigit);
+    nextDigit();
 }
 
 function addEventListener(){
     document.querySelector(".mainCont").addEventListener("click", nextDigit);
 
     document.querySelector(".resetBtnArea").addEventListener("click", resetAll);    
+
+    document.querySelector(".digitCountCont").addEventListener("click", digitCountContClick);
+
+    document.querySelector("#digitCountSelect").addEventListener("change", onDigitCountChange);
+}
+
+function digitCountContClick(){
+    // console.log("digitCountContClick")
+    if (document.querySelector("#selectCont").classList.contains("hdn")){
+    document.querySelector("#selectCont").classList.remove("hdn");
+    } else {
+        document.querySelector("#selectCont").classList.add("hdn");
+    }
 }
 
 function resetAll(){
